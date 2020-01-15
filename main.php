336f2938cfe2d429df0a4a0b8e8371ad62819e91
ChexWarrior <aplehm@gmail.com>
2020-01-15 07:04:54 -0500

    Initial commit 👶

---
+++ main.php
@@ -0,0 +1,17 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use Chexwarrior\Crawler;
+
+/**
+ * 1. Make Login POST
+ * 2. Parse outstading requests from table
+ * 3. Check against save file
+ * 4. Update file with new items
+ * 5. Send notifications
+ */
+unlink('./test.html');
+$crawler = new Crawler();
+$html = $crawler->makePost('username', 'password');
+file_put_contents('./test.html', $html);
---
+++ src/Crawler.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Chexwarrior;
+
+use GuzzleHttp\Client;
+use Psr\Http\Message\ResponseInterface;
+
+class Crawler
+{
+    public const LOGIN_URL = 'https://nypl.illiad.oclc.org/illiad/NYP/illiad.dll';
+
+    public function __construct(?Client $client = null)
+    {
+        if (empty($client)) {
+            $this->client = new Client();
+        } else {
+            $this->client = $client;
+        }
+    }
+
+    public function makePost(string $username, string $password): string
+    {
+        /** @var ResponseInterface */
+        $response = $this->client->request('POST', self::LOGIN_URL, [
+            'form_params' => [
+                'ILLiadForm' => 'Logon',
+                'Username' => $username,
+                'Password' => $password,
+                'SubmitButton' => 'Logon to ILLiad',
+            ],
+        ]);
+
+        return (string) $response->getBody();
+    }
+}
