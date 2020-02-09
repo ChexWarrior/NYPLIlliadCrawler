5c4593232b7bd3ab072e86b0f782c6282adb64c0
Andrew Lehman <aplehm@gmail.com>
2020-02-09 08:13:04 -0500

    Add first implementation of getting outstanding requests

--- src/Parser.php
+++ src/Parser.php
@@ -2,12 +2,29 @@
 
 namespace Chexwarrior;
 
+use Symfony\Component\DomCrawler\Crawler;
+
 /**
  * Parses status of each active transaction
- * 
+ *
  * @package Chexwarrior
  */
 class Parser
 {
+    public function parseOutstandingRequests(string $html)
+    {
+        $crawler = new Crawler($html);
 
+        // Filter to Outstanding Requests Table rows
+        $crawler = $crawler->filter('#content div.default-table table tbody tr');
+        $outstandingRequests = $crawler->each(function (Crawler $tr, $i) {
+            return [
+                'transaction' => $tr->filterXPath('/td[1]'),
+                'title' => $tr->filterXPath('/td[3]'),
+                'status' => $tr->filterXPath('/td[5]'),
+            ];
+        });
+
+        return $outstandingRequests;
+    }
 }
