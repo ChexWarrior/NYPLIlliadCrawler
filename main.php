6c88dfcfdb9df3db2d37f985b15b64fbddc6048b
Andrew Lehman <aplehm@gmail.com>
2020-02-09 08:50:29 -0500

    Update comments

--- main.php
+++ main.php
@@ -5,21 +5,21 @@ require 'vendor/autoload.php';
 use Chexwarrior\Crawler;
 use Chexwarrior\Parser;
 
-/**
- * 1. Make Login POST
- * 2. Parse outstanding requests from table
- * 3. Check against save file
- * 4. Update file with new items
- * 5. Send notifications
- */
-
 $username = $argv[1];
 $password = $argv[2];
 
 $crawler = new Crawler();
 $parser = new Parser();
 
+// 1. Make Login POST
 $html = $crawler->makePost($username, $password);
+
+// 2. Parse oustanding requests from table
 $outstandingRequests = $parser->parseOutstandingRequests($html);
 
-var_dump($outstandingRequests);
+// 3. Check against save file
+
+
+// 4. Update file with new items
+
+// 5. Send notifications
