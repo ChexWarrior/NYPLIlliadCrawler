1ebadb0d79ad7b95f6e77293074d4b458770e56e
Andrew Lehman <aplehm@gmail.com>
2020-02-09 10:33:59 -0500

    Update

--- main.php
+++ main.php
@@ -8,14 +8,15 @@ use Chexwarrior\Parser;
 $username = $argv[1];
 $password = $argv[2];
 
-$crawler = new Crawler();
-$parser = new Parser();
-
 // 1. Make Login POST
+$crawler = new Crawler();
 $html = $crawler->makePost($username, $password);
 
 // 2. Parse oustanding requests from table
+$parser = new Parser();
 $outstandingRequests = $parser->parseOutstandingRequests($html);
+
+echo json_encode($outstandingRequests);
 
 // 3. Check against save file
 
