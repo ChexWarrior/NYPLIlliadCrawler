88f89f27e1c37d28b7b0da032d523164d658f9ee
Andrew Lehman <aplehm@gmail.com>
2020-02-16 07:42:15 -0500

    Load, compare and save requests

--- main.php
+++ main.php
@@ -4,6 +4,9 @@ require 'vendor/autoload.php';
 
 use Chexwarrior\Crawler;
 use Chexwarrior\Parser;
+use Chexwarrior\Saver;
+
+define('SAVE_FILE_PATH', './save.json');
 
 $username = $argv[1];
 $password = $argv[2];
@@ -14,13 +17,19 @@ $html = $crawler->makePost($username, $password);
 
 // 2. Parse oustanding requests from table
 $parser = new Parser();
-$outstandingRequests = $parser->parseOutstandingRequests($html);
+$newRequests = $parser->parseOutstandingRequests($html);
 
-echo json_encode($outstandingRequests);
+echo json_encode($newRequests);
 
 // 3. Check against save file
+$saver = new Saver();
+$savedRequests = $saver->loadFile(SAVE_FILE_PATH);
 
+if (!empty($savedRequests)) {
+    $newRequests = $saver->determineChangedRequests($newRequests, $savedRequests);
+}
 
 // 4. Update file with new items
+$saver->saveFile(SAVE_FILE_PATH, $newRequests);
 
 // 5. Send notifications
